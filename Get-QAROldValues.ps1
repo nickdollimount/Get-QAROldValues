828018e18db25a1dbef6430f3f3e56e090351398
Nick Dollimount <nickdollimount@users.noreply.github.com>
2019-03-05 19:39:53 -0400

    Create Get-QAROldValues.ps1

---
+++ Get-QAROldValues.ps1
@@ -0,0 +1,56 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)]$ARServerFQDN,
+    [Parameter(Mandatory=$True)]$OperationID
+)
+
+function GenericSqlQuery ($Server, $Database, $SQLQuery) {
+    $Connection = New-Object System.Data.SQLClient.SQLConnection
+    $Connection.ConnectionString = "server='$Server';database='$Database';trusted_connection=true;"
+    $Connection.Open()
+    $Command = New-Object System.Data.SQLClient.SQLCommand
+    $Command.Connection = $Connection
+    $Command.CommandText = $SQLQuery
+    $Reader = $Command.ExecuteReader()
+    while ($Reader.Read()) {
+         $Reader.GetValue($1)
+    }
+    $Connection.Close()
+}
+
+function FetchOldValue($_MHSQLAlias, $_MHDatabaseName, $_OperationID){
+    $SQLQuery = $("select data from [" + `
+    $_MHDatabaseName + "].[dbo].[WfSharedOperations] inner join [" + `
+    $_MHDatabaseName + "].[dbo].[WfOperationValues] on [" + $_MHDatabaseName + "].[dbo].[WfSharedOperations].guid = [" + `
+    $_MHDatabaseName + "].[dbo].[WfOperationValues].operation where [" + `
+    $_MHDatabaseName + "].[dbo].[WfOperationValues].value_short = '" + $_OperationID + "'")
+
+    [xml] $xmlResults = GenericSqlQuery $_MHSQLAlias $_MHDatabaseName $SQLQuery
+    $oldValues = $xmlResults.Operation.PreviousAttributes.PreviousAttribute
+
+    $oldValuesObject = @()
+
+    $oldValues | ForEach-Object{
+        $properties = @{
+            "Attribute" = $_.name
+            "Old Value" = $_.values.value
+        }
+    $tempObject = New-Object PSObject -Property $properties
+    $oldValuesObject += $tempObject
+    }
+
+    return $oldValuesObject
+}
+
+function Main($_ARServerFQDN, $_OperationID){
+    Connect-QADService -Service $_ARServerFQDN -Proxy | Out-Null
+
+    $ARService = Get-QADObject -SearchRoot "Configuration/Server Configuration/Administration Services" `
+        -DontUseDefaultIncludedProperties `
+        -IncludedProperties edsaMHSQLAlias,edsaMHDatabaseName,edsaEdmServiceComputerName `
+        -Type edsARService | Where-Object{$_.edsaEdmServiceComputerName -eq $_ARServerFQDN}
+
+    FetchOldValue $ARService.edsaMHSQLAlias $ARService.edsaMHDatabaseName $_OperationID $_AttributeName
+}
+
+Main $ARServerFQDN $OperationID
